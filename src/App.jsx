2cac9cce1345c4a6d4429b9ff91658a14b3c3538
karixa-l <kalebedeva@gmail.com>
2025-03-03 14:04:37 +0800

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://k-g.wuaze.com/wp-json/wp/v2/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data))
+      .catch((error) => console.error("Error fetching posts:", error));
+  }, []);
+
   return (
     <div className="homepage">
-      <h1>Welcome to My Website</h1>
+      <h1>Latest Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h2 dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
+            <p dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }} />
+            <a href={post.link} target="_blank" rel="noopener noreferrer">
+              Read More
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }

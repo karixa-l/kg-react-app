18d8dd38a63d6d9f45d805106b03e4089a211b93
karixa-l <kalebedeva@gmail.com>
2025-03-07 22:31:48 +0800

    vid

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-import { useState, useEffect } from "react";
-import "./App.css";
-
-function App() {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((data) => setPosts(data))
-      .catch((error) => console.error("Error fetching posts:", error));
-  }, []);
-
+const VideoBackground = () => {
   return (
-    <div className="homepage">
-      <h1>Latest Posts</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
-            <a href={`https://jsonplaceholder.typicode.com/posts/${post.id}`} target="_blank" rel="noopener noreferrer">
-              Read More
-            </a>
-          </li>
-        ))}
-      </ul>
+    <div className="video-container">
+      <iframe
+        src="https://www.youtube.com/embed/xNwQF0kYJjM?si=gNiYbEoS1iKxQLBQ&autoplay=1&mute=1&loop=1&playlist=xNwQF0kYJjM"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="autoplay; encrypted-media; fullscreen"
+        allowFullScreen
+      ></iframe>
     </div>
   );
-}
+};
 
-export default App;
+export default VideoBackground;
 

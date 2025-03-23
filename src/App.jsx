365a442caf87901ba36a33d5fa597962b5f19236
karixa-l <kalebedeva@gmail.com>
2025-03-23 14:47:16 +0800

    n2

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const VideoBackground = () => {
   return (
     <div className="video-container">
       <iframe
-        src="https://www.youtube.com/embed/xNwQF0kYJjM?si=Dmq4PVMr-vpO_3D8&controls=0&autoplay=1&mute=1&loop=1&playlist=xNwQF0kYJjM"
+        src="https://www.youtube.com/embed/xNwQF0kYJjM?si=Dmq4PVMr-vpO_3D8&autoplay=1&mute=1&loop=1&playlist=xNwQF0kYJjM&modestbranding=1&controls=0&showinfo=0&rel=0"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
@@ -18,14 +18,39 @@ const VideoBackground = () => {
   );
 };
 
-function App() {
+const OverlayBlocks = () => {
   return (
-    <div>
-      <VideoBackground />
-      <h1>Welcome to My Website</h1>
-      <button>Click me</button>
+    <div className="overlay-container">
+      <div className="grid-block"><h3>Block 1</h3></div>
+      <div className="grid-block"><h3>Block 2</h3></div>
+      <div className="grid-block"><h3>Block 3</h3></div>
+      <div className="grid-block"><h3>Block 4</h3></div>
     </div>
   );
-}
+};
+const ScrollSection = () => {
+  return (
+    <div className="scroll-section">
+      <h1>scroll.</h1>
+      <ul>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <li key={index}>
+            <img src={`https://picsum.photos/900/1200?random=${index + 1}`} alt="" />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="app">
+      <VideoBackground />
+      <OverlayBlocks />
+      <ScrollableGallery />
+    </div>
+  );
+};
 
 export default App;
